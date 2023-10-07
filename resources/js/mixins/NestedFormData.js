dd35e45010b4608cb3dc6b7bedd3073b148bcfdc
Claudio Pennati <claudio.pennati@gmail.com>
2023-10-07 14:22:18 +0200

    fix bug #10 (#11)

--- resources/js/mixins/NestedFormData.js
+++ resources/js/mixins/NestedFormData.js
@@ -1,4 +1,59 @@
-import InlineFormData from '@/fields/Form/InlineFormData';
+class NestedFormData {
+    constructor(attribute, formData) {
+        this.attribute = attribute
+        this.parentFormData = formData
+        this.localFormData = new FormData()
+    }
+
+    append(name, ...args) {
+        this.localFormData.append(name, ...args)
+        this.parentFormData.append(this.name(name), ...args)
+    }
+
+    delete(name) {
+        this.localFormData.delete(name)
+        this.parentFormData.delete(this.name(name))
+    }
+
+    entries() {
+        return this.localFormData.entries()
+    }
+
+    get(name) {
+        return this.localFormData.get(name)
+    }
+
+    getAll(name) {
+        return this.localFormData.getAll(name)
+    }
+
+    has(name) {
+        return this.localFormData.has(name)
+    }
+
+    keys() {
+        return this.localFormData.keys()
+    }
+
+    set(name, ...args) {
+        this.localFormData.set(name, ...args)
+        this.parentFormData.set(this.name(name), ...args)
+    }
+
+    values() {
+        return this.localFormData.values()
+    }
+
+    name(attribute) {
+        let [name, ...nested] = attribute.split('[')
+
+        if (!_.isNil(nested) && nested.length > 0) {
+        return `${this.attribute}[${name}][${nested.join('[')}`
+        }
+
+        return `${this.attribute}[${attribute}]`
+    }
+}
 
 export default {
     methods: {
@@ -9,7 +64,7 @@ export default {
             let formIndex = 0;
             _.each(resources, (resource, index) => {
                 if (!resource.loading) {
-                    _.tap(new InlineFormData(`${attribute}[${formIndex}]`, formData), resourceForm => {
+                    _.tap(new NestedFormData(`${attribute}[${formIndex}]`, formData), resourceForm => {
                         if (!withDeleted && resource.isNestedSoftDeleted) {
                             return;
                         }
