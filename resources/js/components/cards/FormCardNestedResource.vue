123beb135ae0de258a2f0d4a18e6e092fd08d8b8
Claudio Pennati <claudio.pennati@gmail.com>
2023-12-07 15:09:06 +0100

    wip

--- resources/js/components/cards/FormCardNestedResource.vue
+++ resources/js/components/cards/FormCardNestedResource.vue
@@ -54,6 +54,7 @@
     import { uid } from 'uid/single';
     import { mapProps } from '@/mixins';
     import LoadingButton from '@/components/Buttons/LoadingButton';
+    import DangerButton from '@/components/Buttons/DangerButton';
 
     import InteractsWithResource from '../../mixins/InteractsWithResource';
 
@@ -61,7 +62,7 @@
         emits: ['run-action', 'field-changed', 'file-deleted', 'file-upload-started', 'file-upload-finished'],
 
         mixins: [InteractsWithResource],
-        components: { LoadingButton },
+        components: { LoadingButton, DangerButton },
         props: {
             ...mapProps(['showHelpText', 'viaResourceId', 'viaRelationship']),
 
